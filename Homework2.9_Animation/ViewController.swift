3cc1f28433cb7a8bc840782b54e4df88cbb2fe1a
Anastasia <pavlova.a.s@inbox.ru>
2022-06-16 00:02:31 +0300

    add

--- Homework2.9_Animation/ViewController.swift
+++ Homework2.9_Animation/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
