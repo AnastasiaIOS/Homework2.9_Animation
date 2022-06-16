68f602ef253b00a92b653bfcc487e232fb3d9b13
Anastasia <pavlova.a.s@inbox.ru>
2022-06-16 23:56:29 +0300

    add

--- Homework2.9_Animation/Model/Animation Model.swift
+++ Homework2.9_Animation/Model/Animation Model.swift
@@ -15,4 +15,52 @@ struct Animation {
 
 extension Animation {
     
+    static func getParametrsOfAnimation() -> [Animation] {
+        
+        var massiveOfParametrs: [Animation] = []
+        
+        let massiaveOfPresets = groupMassive.preset
+        let massiveOfCurves = groupMassive.curve
+        let massiveOfForces = groupMassive.force
+        let massiveOfDurations = groupMassive.duration
+        let massiveOfDelays = groupMassive.delay
+        
+        
+        for _ in 1... {
+        let groupOfParametrs = Animation(preset: massiaveOfPresets.randomElement(),
+                                          curve: massiveOfCurves.randomElement(),
+                                          force: massiveOfForces.randomElement(),
+                                       duration: massiveOfDurations.randomElement(),
+                                          delay: massiveOfDelays.randomElement())
+            
+            massiveOfParametrs.append(groupOfParametrs)
+        }
+         return massiveOfParametrs
+      
+    }
+    
 }
+
+/*extension Person {
+    static func getPerson() -> [Person] {
+        
+        var massiveOfPersons: [Person] = []
+        
+        let massiveOfNames = groupMassive.names.shuffled()
+        let massiveOfSurnames = groupMassive.surnames.shuffled()
+        let massiveOfPhones = groupMassive.phones.shuffled()
+        let massiveOfEmails = groupMassive.emails.shuffled()
+        
+        let numberOfPersons = min(massiveOfNames.count, massiveOfSurnames.count, massiveOfPhones.count, massiveOfEmails.count)
+        
+        for number in 1...numberOfPersons {
+            let person = Person(name: massiveOfNames[number - 1] ,
+                                surname: massiveOfSurnames[number - 1],
+                                phone: massiveOfPhones[number - 1],
+                                email: massiveOfEmails[number - 1])
+            
+            massiveOfPersons.append(person)
+        }
+        return massiveOfPersons
+    }
+}*/
