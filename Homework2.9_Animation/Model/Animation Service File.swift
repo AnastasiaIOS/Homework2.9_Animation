8ba590a4bb0c650a10c7d98f9c3420b37e122f5d
Anastasia <pavlova.a.s@inbox.ru>
2022-06-16 22:49:25 +0300

    add

--- Homework2.9_Animation/Model/Animation Service File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Animation Service File.swift
-//  Homework2.9_Animation
-//
-//  Created by Anastasia on 16.06.2022.
-//
-
-import Foundation
---
+++ Homework2.9_Animation/Service/Animation Service File.swift
@@ -0,0 +1,19 @@
+//
+//  Animation Service File.swift
+//  Homework2.9_Animation
+//
+//  Created by Anastasia on 16.06.2022.
+//
+
+class AnimationService {
+    
+    
+    let preset = ["shake", "pop", "morph", "squeeze", "wobble", "slideLeft", "slideDown", "fadeInLeft", "zoomIn", "flash"]
+    let curve = ["spring", "linear", "easeIn", "easeOut", "easeInOut"]
+    let force = ["947382", "423422", "342342", "34234332", "4555555", "0329403"]
+    let duration = ["qqq@bk.ru", "eew@bk.ru", "omo@bk.ru", "qqq@bk.ru", "wwww@bk.ru", "pppp@bk.ru"]
+    let delay = ["qqq@bk.ru", "eew@bk.ru", "omo@bk.ru", "qqq@bk.ru", "wwww@bk.ru", "pppp@bk.ru"]
+
+}
+
+let groupMassive = AnimationService()
