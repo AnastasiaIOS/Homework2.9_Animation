8205397f88d4420cf2d68657e875cf112ba6e907
Anastasia <pavlova.a.s@inbox.ru>
2022-06-16 22:35:35 +0300

    add

---
+++ Homework2.9_Animation/Model/Animation Model.swift
@@ -0,0 +1,18 @@
+//
+//  Animation Model.swift
+//  Homework2.9_Animation
+//
+//  Created by Anastasia on 16.06.2022.
+//
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
+
+extension Animation {
+    
+}
